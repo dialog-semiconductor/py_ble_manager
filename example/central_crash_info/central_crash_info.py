36f6acdb96f9a3f496d8a3b876db605f639f3e7a
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-04-18 18:11:38 -0400

    parsing reset info

--- example/central_crash_info/central_crash_info.py
+++ example/central_crash_info/central_crash_info.py
@@ -67,33 +67,47 @@ class DebugCrashInfoSvc():
         self.tx_user_desc = ble.GattcItem()
         self.tx_ccc = ble.GattcItem()
 
+
 class CortexM0StackFrame():
-    def __init__(self):
-        self.r0 = 0
-        self.r1 = 0
-        self.r2 = 0
-        self.r3 = 0
-        self.r12 = 0
-        self.LR = 0
-        self.return_address = 0
-        self.xPSR = 0
+    def __init__(self, data: bytes = None):
+        self.r0 = self.reg_from_bytes(data[0:4]) if data else 0
+        self.r1 = self.reg_from_bytes(data[4:8]) if data else 0
+        self.r2 = self.reg_from_bytes(data[8:12]) if data else 0
+        self.r3 = self.reg_from_bytes(data[12:16]) if data else 0
+        self.r12 = self.reg_from_bytes(data[16:20]) if data else 0
+        self.LR = self.reg_from_bytes(data[20:24]) if data else 0
+        self.return_address = self.reg_from_bytes(data[24:28]) if data else 0
+        self.xPSR = self.reg_from_bytes(data[28:32]) if data else 0
+
+    def reg_from_bytes(self, data: bytes):
+        return hex((data[0]) | (data[1] << 8) | (data[2] << 16) | (data[3] << 24))
+    
+
+    ''' TODO ensure print in hex
+    def __repr__(self):
+        return_string = f"{type(self).__name__}(data_valid={self.data_valid}, epoch={self.epoch}, " + \
+                        f"fault_handler={self.fault_handler}, stack_frame={self.stack_frame}, " + \
+                        f"num_of_call_vals={self.num_of_call_vals}, call_trace={self.call_trace}, " + \
+
+        return return_string
+    '''
 
 
 
 class DCI_REST_REASON(IntEnum):
-    NONE = auto()
-    POR = auto()
+    POR = 0
     HW = auto()
     SW = auto()
     WDOG = auto()
     LAST = auto()
+    NONE = auto()
 
 
 class DCI_LAST_FAULT_HANDLER(IntEnum): 
-    NONE = auto()
-    HARDFAULT = auto()
+    HARDFAULT = 0
     NMI = auto()
     PLATFORM_RESET = auto()
+    NONE = auto()
 
 
 class DciFaultInfo():
@@ -103,21 +117,27 @@ class DciFaultInfo():
         self.fault_handler: DCI_LAST_FAULT_HANDLER = DCI_LAST_FAULT_HANDLER.NONE 
         self.stack_frame = CortexM0StackFrame()
         self.num_of_call_vals: int = 0
-        self.call_trace: list[int] = []
+        self.call_trace: list[bytes] = []
+
+    def __repr__(self):
+        return_string = f"{type(self).__name__}(data_valid={self.data_valid}, epoch={self.epoch}, " + \
+                        f"fault_handler={self.fault_handler.name}, stack_frame={self.stack_frame}, " + \
+                        f"num_of_call_vals={self.num_of_call_vals}, call_trace={self.call_trace}, "
+        return return_string
 
 
 class DciData():
     def __init__(self):
         self.last_reset_reason: DCI_REST_REASON = DCI_REST_REASON.NONE
         self.num_resets: int = 0
-        self.fault_data: DciFaultInfo = DciFaultInfo()
+        self.fault_data: list[DciFaultInfo] = []
 
-    
     def __repr__(self):
-        return_string = f"last_reset_reason={self.last_reset_reason}"
-        
+        return_string = f"{type(self).__name__}(last_reset_reason={self.last_reset_reason}, num_resets={self.num_resets}, " + \
+                        f"fault_data={self.fault_data}"
+
         return return_string
-    
+
 
 async def console(ble_command_q: asyncio.Queue, ble_response_q: asyncio.Queue):
         commands = ['GAPSCAN',
@@ -405,10 +425,16 @@ class BleController():
         length = len(data)
         self.reset_data.last_reset_reason = DCI_REST_REASON(data[0])
         self.reset_data.num_resets = data[1]
-        self.reset_data.fault_data.data_valid = bool(data[2])
-        self.reset_data.fault_data.epoch = data[3:7]
-        self.reset_data.fault_data.fault_handler = data[7:11]
-        self.reset_data.fault_data.stack_frame.r0 = data[11:15]
+
+        for i in range(self.reset_data.num_resets):
+            self.reset_data.fault_data.append(DciFaultInfo())
+            self.reset_data.fault_data[i].data_valid = bool(data[2])
+            # Todo need to reverse
+            self.reset_data.fault_data[i].epoch = (data[3]) | (data[4] << 8) | (data[5] << 16) | (data[6] << 24)
+            self.reset_data.fault_data[i].fault_handler = DCI_LAST_FAULT_HANDLER(data[7])
+            
+            self.reset_data.fault_data[i].stack_frame = CortexM0StackFrame(data[8:])
+
 
         print(f"reset data: {self.reset_data}")
             
