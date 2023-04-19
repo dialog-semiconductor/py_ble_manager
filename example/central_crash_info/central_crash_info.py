9803c0331fe8b46ae4de3a4387dbeba0bd712d66
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-04-19 18:12:30 -0400

    separate dci module

--- example/central_crash_info/central_crash_info.py
+++ example/central_crash_info/central_crash_info.py
@@ -7,6 +7,7 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.patch_stdout import patch_stdout
 import time
+from debug_crash_info import DciData, DciFaultInfo, CortexM0StackFrame, ResetData, DCI_LAST_FAULT_HANDLER, DCI_REST_REASON, DCI_SVC_COMMAND
 
 # TODO rethink relative import
 import sys
@@ -30,18 +31,6 @@ USER_DESC_UUID_STR = "2901"
 DEVICE_NAME_HANDLE = 3
 
 
-class GATT_EVENT(IntEnum):
-    GATT_EVENT_NONE = 0x0000
-    GATT_EVENT_NOTIFICATION = 0x0001
-    GATT_EVENT_INDICATION = 0x0002
-
-
-class DCI_SVC_COMMAND(IntEnum):
-    DCI_SERVICE_COMMAND_NONE = 0
-    DCI_SERVICE_COMMAND_GET_ALL_DATA = auto()
-    DCI_SERVICE_COMMAND_GET_NUM_RESETS = auto()
-
-
 class FETCH_DATA_STATE(IntEnum):
     FETCH_DATA_NONE = auto()
     FETCH_DATA_CONNECT = auto()
@@ -54,14 +43,11 @@ class FETCH_DATA_STATE(IntEnum):
     FETCH_DATA_DONE = auto()
     FETCH_DATA_ERROR = auto()
 
-#crash_info_uuid: ble.AttUuid = self.uuid_from_str(DEBUG_CRASH_INFO_SVC_UUID_STR)
 
-
-class ResetData():
-    def __init__(self):
-        self.command = DCI_SVC_COMMAND.DCI_SERVICE_COMMAND_NONE
-        self.len = 0
-        self.data = bytes()
+class GATT_EVENT(IntEnum):
+    GATT_EVENT_NONE = 0x0000
+    GATT_EVENT_NOTIFICATION = 0x0001
+    GATT_EVENT_INDICATION = 0x0002
 
 
 class DebugCrashInfoSvc():
@@ -71,99 +57,26 @@ class DebugCrashInfoSvc():
         self.rx_user_desc = ble.GattcItem()
         self.tx = ble.GattcItem()
         self.tx_user_desc = ble.GattcItem()
-        self.tx_ccc = ble.GattcItem()
-
-
-class CortexM0StackFrame():
-    def __init__(self, data: bytes = None):
-        self.r0 = self.reg_from_bytes(data[0:4]) if data else 0
-        self.r1 = self.reg_from_bytes(data[4:8]) if data else 0
-        self.r2 = self.reg_from_bytes(data[8:12]) if data else 0
-        self.r3 = self.reg_from_bytes(data[12:16]) if data else 0
-        self.r12 = self.reg_from_bytes(data[16:20]) if data else 0
-        self.LR = self.reg_from_bytes(data[20:24]) if data else 0
-        self.return_address = self.reg_from_bytes(data[24:28]) if data else 0
-        self.xPSR = self.reg_from_bytes(data[28:32]) if data else 0
-
-    def reg_from_bytes(self, data: bytes):
-        return int.from_bytes(data, 'little')
-    
-
-    # TODO ensure print in hex
-    def __repr__(self):
-        return_string = f"{type(self).__name__}(r0=0x{self.r0:08x}, r1=0x{self.r1:08x}, " + \
-                        f"r2=0x{self.r2:08x}, r3=0x{self.r3:08x}, " + \
-                        f"r12=0x{self.r12:08x}, LR=0x{self.LR:08x}, " + \
-                        f"return_address=0x{self.return_address:08x}, xPSR=0x{self.xPSR:08x})"
-
-        return return_string
-
-
-class DCI_REST_REASON(IntEnum):
-    POR = 0
-    HW = auto()
-    SW = auto()
-    WDOG = auto()
-    LAST = auto()
-    NONE = auto()
-
-
-class DCI_LAST_FAULT_HANDLER(IntEnum):
-    HARDFAULT = 0
-    NMI = auto()
-    PLATFORM_RESET = auto()
-    NONE = auto()
-
-
-class DciFaultInfo():
-    def __init__(self):
-        self.data_valid: bool = False
-        self.epoch: int = 0
-        self.fault_handler: DCI_LAST_FAULT_HANDLER = DCI_LAST_FAULT_HANDLER.NONE 
-        self.stack_frame = CortexM0StackFrame()
-        self.num_of_call_vals: int = 0
-        self.call_trace: list[int] = []
-
-    def __repr__(self):
-        return_string = f"{type(self).__name__}(data_valid={self.data_valid}, epoch={self.epoch}, " + \
-                        f"fault_handler={self.fault_handler.name}, stack_frame={self.stack_frame}, " + \
-                        f"num_of_call_vals={self.num_of_call_vals}, call_trace="
-        for i in range(self.num_of_call_vals):
-            return_string += f"0x{self.call_trace[i]:08x}, "
-        return_string = return_string[:-2]
-        return_string += ")"
-        return return_string
-
-
-class DciData():
-    def __init__(self):
-        self.last_reset_reason: DCI_REST_REASON = DCI_REST_REASON.NONE
-        self.num_resets: int = 0
-        self.fault_data: list[DciFaultInfo] = []
-
-    def __repr__(self):
-        return_string = f"{type(self).__name__}(last_reset_reason={self.last_reset_reason}, num_resets={self.num_resets}, " + \
-                        f"fault_data={self.fault_data}"
-
-        return return_string
+        self.tx_ccc = ble.GattcItem()    
 
 
 async def console(ble_command_q: asyncio.Queue, ble_response_q: asyncio.Queue):
-        commands = ['GAPSCAN',
-                    'GETALLRESETDATA']
-        commands.sort()
-        word_completer = WordCompleter(commands, ignore_case=True)
+    commands = ['GAPSCAN',
+                'GETALLRESETDATA',
+                'EXIT']
+    commands.sort()
+    word_completer = WordCompleter(commands, ignore_case=True)
 
-        session = PromptSession(completer=word_completer)
-        while True:
-            with patch_stdout():
-                input = await session.prompt_async('>>> ')
-                ble_command_q.put_nowait(input)
-                response = await ble_response_q.get()
-                print(f"<<< {response}")
-                #if input == 'GAPSCAN' and response == "OK":
-                #    await scan_complete_evt.wait()
-                # TODO wait for EVENT from ble_task before accepting additional input (eg scan done)
+    session = PromptSession(completer=word_completer)
+    while True:
+        with patch_stdout():
+            input = await session.prompt_async('>>> ')
+            ble_command_q.put_nowait(input)
+            response = await ble_response_q.get()
+            print(f"<<< {response}")
+            #if input == 'GAPSCAN' and response == "OK":
+            #    await scan_complete_evt.wait()
+            # TODO wait for EVENT from ble_task before accepting additional input (eg scan done)
 
 
 class BleController():
@@ -203,7 +116,8 @@ class BleController():
         log_to_file_task = asyncio.create_task(self.log_file.write(string + "\r"))
         self.log_tasks.add(log_to_file_task)
         log_to_file_task.add_done_callback(self.log_tasks.discard)
-        # TODO create task to write str to file
+
+        # TODO consider asyncio.sleep here to give the logging task a chance to write the data to the file
 
     def bd_addr_to_str(self, bd: ble.BdAddress) -> str:
         return_string = ""
@@ -299,6 +213,11 @@ class BleController():
                     if error == ble.BLE_ERROR.BLE_STATUS_OK:
                         self.fetch_state = FETCH_DATA_STATE.FETCH_DATA_CONNECT
 
+                case "EXIT":
+                    self.log_file.close()
+                    self.fetch_state = FETCH_DATA_STATE.FETCH_DATA_NONE
+                    sys.exit(0)
+
                 case _:
                     pass
 
@@ -332,7 +251,6 @@ class BleController():
                     await self.central.handle_event_default(evt)
 
             self.process_fetch_state(evt)
-
 
     def process_fetch_state(self, evt: ble.BleEventBase):
 
@@ -395,8 +313,11 @@ class BleController():
             case FETCH_DATA_STATE.FETCH_DATA_DISCONNECT:
                 if (evt.evt_code == ble.BLE_EVT_GAP.BLE_EVT_GAP_DISCONNECTED):
                     self.log("Disconnected")
+
+                    # TODO move these ?
                     self.log_file.close()
                     self.fetch_state = FETCH_DATA_STATE.FETCH_DATA_NONE
+                    sys.exit(0)
 
         if self.fetch_state == FETCH_DATA_STATE.FETCH_DATA_ERROR:
             self.log("Errorrrrr")
@@ -617,6 +538,8 @@ if __name__ == "__main__":
     try:
         asyncio.run(main(args.com_port))
     except KeyboardInterrupt:
+        # TODO A BLE connection will remain active if the keyboard interrupt happens during the connection 
+        # For now restarting the script will reset the BLE stack for the central device, closing the connection
         pass
     finally:
         print("Exiting")
