3a5d023b3edd17afee0fb2ab8b0842b963a157cc
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-05-09 08:01:22 -0400

    wip port periph example to threads

--- examples/peripheral/main_periph.py
+++ examples/peripheral/main_periph.py
@@ -1,16 +1,14 @@
 import argparse
-import asyncio
-
+import threading
+import queue
+import time
 # TODO rethink relative import
 # TODO rethink relative import
 import sys
-from pathlib import Path
-HERE = Path(__file__).parent
-sys.path.append(str(HERE / '../../'))
+import python_gtl_thread as ble
 
-import ble_devices as ble
-
-from services.CustomBleService import CustomBleService, CustomBleServiceCallbacks
+'''
+from ble.CustomBleService import CustomBleService, CustomBleServiceCallbacks
 
 
 async def app_char1_read_callback(svc: CustomBleService, conn_idx: int):
@@ -35,93 +33,80 @@ async def app_char2_write_callback(svc: CustomBleService, conn_idx: int, value: 
 async def app_char3_ccc_changed_callback(svc: CustomBleService, conn_idx: int, value: int):
     print(f"app_char3_ccc_changed_callback. conn_idx={conn_idx}, value={value}")
 
-
-async def user_main(sample_q: asyncio.Queue):
-    elapsed = 0
-    delay = 1
-    while True:
-        await asyncio.sleep(delay)
-        elapsed += delay
-        sample_q.put_nowait(elapsed)
-        # print(f"User Main. elapsed={elapsed}")
-
+'''
 
 async def main(com_port: str):
 
-    sample_q = asyncio.Queue()
-    await asyncio.gather(user_main(sample_q), ble_task(com_port, sample_q))
+    sample_q = queue.Queue()
 
 
-async def ble_task(com_port: str, sample_q: asyncio.Queue):
+    ble_thread = threading.Thread(target=threading.Thread(target=ble_task))
+    ble_thread.start()
+
+    while True:
+        if ble_thread.is_alive():
+            time.sleep(1)
+        else:
+            return)
+
+
+
+
+async def ble_task(com_port: str, sample_q: queue.Queue):
 
     periph = ble.BlePeripheral(com_port, gtl_debug=True)
     await periph.init()
     await periph.start()
 
-    my_service_callbacks = CustomBleServiceCallbacks(app_char1_read_callback,
-                                                     app_char1_write_callback,
-                                                     app_char2_write_callback)
-    my_service = CustomBleService(my_service_callbacks)
-    my_service.init()
+    #my_service_callbacks = CustomBleServiceCallbacks(app_char1_read_callback,
+    #                                                 app_char1_write_callback,
+    #                                                 app_char2_write_callback)
+    #my_service = CustomBleService(my_service_callbacks)
+    #my_service.init()
 
-    await periph.register_service(my_service)
-    await my_service.set_char2_value((0x8692).to_bytes(2, 'little'))
-    await my_service.set_char3_user_desc_value(b"Hello")
+    #await periph.register_service(my_service)
+    #await my_service.set_char2_value((0x8692).to_bytes(2, 'little'))
+    #await my_service.set_char3_user_desc_value(b"Hello")
 
     periph.set_io_cap(ble.GAP_IO_CAPABILITIES.GAP_IO_CAP_DISP_YES_NO)
 
     periph.set_advertising_interval(20, 30)
     await periph.start_advertising()
 
-    timer_read_task = asyncio.create_task(sample_q.get(), name='sample_q_Read')
-    ble_event_task = asyncio.create_task(periph.get_event(), name='GetBleEvent')
-    pending = [timer_read_task, ble_event_task]
+    #timer_read_task = asyncio.create_task(sample_q.get(), name='sample_q_Read')
+    #ble_event_task = asyncio.create_task(periph.get_event(), name='GetBleEvent')
+    #pending = [timer_read_task, ble_event_task]
 
     while True:
-        done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
+        #done, pending = await asyncio.wait(pending, return_when=asyncio.FIRST_COMPLETED)
+        evt = periph.get_event()
+       
+        if evt is not None:
+            handled = await periph.service_handle_event(evt)
+            if not handled:
+                match evt.evt_code:
+                    case ble.BLE_EVT_GAP.BLE_EVT_GAP_DISCONNECTED:
+                        await periph.start_advertising()
+                    case ble.BLE_EVT_GAP.BLE_EVT_GAP_PAIR_REQ:
+                        evt: ble.BleEventGapPairReq
+                        print(f"BleEventGapPairReq. evt={evt}")
+                        await periph.pair_reply(evt.conn_idx, True, evt.bond)
+                    case ble.BLE_EVT_GAP.BLE_EVT_GAP_NUMERIC_REQUEST:
+                        await periph.numeric_reply(evt.conn_idx, True)
+                    case _:
+                        await periph.handle_event_default(evt)
+           
 
-        for task in done:
-
-            # Every second a "sample" is received from user_main
-            if task is timer_read_task:
-                sample: int = task.result()
-                if my_service.ccc == 1:  # TODO this should come from changed callback
-                    # TODO how to get conn_idx? Notify all connected method?
-                    error = await my_service.notify_char3(0, sample.to_bytes(4, byteorder='little'))
-                    print(f"Notification sent. sample = {sample}, error = {error}")
-
-                timer_read_task = asyncio.create_task(sample_q.get(), name='sample_q_Read')
-                pending.add(timer_read_task)
-
-            # Handle and BLE events that hace occurred
-            elif task is ble_event_task:
-                evt: ble.BleEventBase = task.result()  # TODO how does timeout error affect result
-                if evt is not None:
-                    handled = await periph.service_handle_event(evt)
-                    if not handled:
-                        match evt.evt_code:
-                            case ble.BLE_EVT_GAP.BLE_EVT_GAP_DISCONNECTED:
-                                await periph.start_advertising()
-                            case ble.BLE_EVT_GAP.BLE_EVT_GAP_PAIR_REQ:
-                                evt: ble.BleEventGapPairReq
-                                print(f"BleEventGapPairReq. evt={evt}")
-                                await periph.pair_reply(evt.conn_idx, True, evt.bond)
-                            case ble.BLE_EVT_GAP.BLE_EVT_GAP_NUMERIC_REQUEST:
-                                await periph.numeric_reply(evt.conn_idx, True)
-                            case _:
-                                await periph.handle_event_default(evt)
-
-                print(f"Main rx'd event: {evt}. hanlded={handled} \n")
-
-                ble_event_task = asyncio.create_task(periph.get_event(), name='GetBleEvent')
-                pending.add(ble_event_task)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='main_peripheral',
-                                     description='BLE Perioheral')
+    parser = argparse.ArgumentParser(prog='main_central',
+                                     description='BLE Central AT Command CLI')
 
     parser.add_argument("com_port")
 
     args = parser.parse_args()
 
-    asyncio.run(main(args.com_port))
+    try:
+        main(args.com_port)
+    except KeyboardInterrupt:
+        print("Keyborard")
