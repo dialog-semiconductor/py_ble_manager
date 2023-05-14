2d0043d1b6c6a802589bbbc93610a76ffe30e2c0
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-05-14 18:45:51 -0400

    wip BleDeviceBase docstrings

--- src/python_gtl_thread/ble_devices/BleDeviceBase.py
+++ src/python_gtl_thread/ble_devices/BleDeviceBase.py
@@ -14,7 +14,18 @@ from ..serial_manager.SerialStreamManager import SerialStreamManager
 
 
 class BleDeviceBase():
+    """Base BLE device class. Implements functionality common to BLE central and BLE peripheral devices
+
+    :param com_port: COM port of the development kit
+    :type com_port: str
+    :param ble_config: BLE configuration to use, defaults to BleConfigDefault(BLE_DEVICE_TYPE.CENTRAL)
+    :type ble_config: BleConfigDefault, optional
+    :param gtl_debug: enable or disable GTL debugging, defaults to False
+    :type gtl_debug: bool, optional
+    """
     def __init__(self, com_port: str, config: BleConfigDefault = BleConfigDefault(), gtl_debug: bool = False):
+        """Constructor
+        """
         app_command_q = queue.Queue()
         app_resposne_q = queue.Queue()
         app_event_q = queue.Queue()
@@ -38,6 +49,12 @@ class BleDeviceBase():
         self._services: list[BleServiceBase] = []
 
     def _ble_reset(self) -> BLE_ERROR:
+        """Reset BLE module
+
+        :return: result code
+        :rtype: BLE_ERROR
+        """
+
         command = BleMgrCommonResetCmd()
         response: BleMgrCommonResetRsp = self._ble_manager.cmd_execute(command)
         return response.status
@@ -60,9 +77,23 @@ class BleDeviceBase():
         :return: result code
         :rtype: BLE_ERROR
         """
+
         return self._ble_gap.conn_param_update(conn_idx, conn_params)
 
     def conn_param_update_reply(self, conn_idx: int, accept: bool) -> BLE_ERROR:
+        """Reply to a connection parameter update request
+
+        This call should be used to reply to a connection parameter update request event
+        (:py:class:`~python_gtl_thread.ble_api.BleCommon.BLE_EVT_GAP.BLE_EVT_GAP_CONN_PARAM_UPDATE_REQ`) message.
+
+        :param conn_idx: connection index
+        :type conn_idx: int
+        :param accept: accept flag (True to accept, False to reject)
+        :type accept: bool
+        :return: result code
+        :rtype: BLE_ERROR
+        """
+
         return self._ble_gap.conn_param_update_reply(conn_idx, accept)
 
     def init(self) -> None:
@@ -79,16 +110,52 @@ class BleDeviceBase():
         # except concurrent.futures.TimeoutError as e:
         #    raise e
 
-    def get_event(self, timeout=None) -> BleEventBase:
+    def get_event(self, timeout: int = None) -> BleEventBase:
+        """Get event from BLE event queue
+
+        :param timeout: time (in seconds) to wait for an event on the queue, defaults to None
+        :type timeout: int, optional
+        :return: an event from the queue. `None` if queue empty when `timeout` is exceeded
+        :rtype: BleEventBase
+        """
+
         return self._ble_manager.mgr_event_queue_get(timeout)
 
-    def numeric_reply(self, conn_idx: int, accept: bool):
+    def numeric_reply(self, conn_idx: int, accept: bool) -> BLE_ERROR:
+        """Respond to a numeric comparison request
+
+        Respond to a :py:class:`~python_gtl_thread.ble_api.BleCommon.BLE_EVT_GAP.BLE_EVT_GAP_NUMERIC_REQUEST` event.
+
+        :param conn_idx: connection index
+        :type conn_idx: int
+        :param accept: accept flag
+        :type accept: bool
+        :return: result code
+        :rtype: BLE_ERROR
+        """
         return self._ble_gap.numeric_reply(conn_idx, accept)
 
     def set_io_cap(self, io_cap: GAP_IO_CAPABILITIES) -> BLE_ERROR:
+        """Set the I/O capabilities of the device
+
+        Set the Input/Output Capabilities of the device (combined with the peer's I/O capabilities, this
+        will determine which pairing algorithm will be used).
+
+        :param io_cap: new IO capabilities
+        :type io_cap: GAP_IO_CAPABILITIES
+        :return: result code
+        :rtype: BLE_ERROR
+        """
         return self._ble_manager.set_io_cap(io_cap)
 
     def start(self, role: BLE_GAP_ROLE) -> BLE_ERROR:
+        """Start the BLE module as a central device
+
+        :param role: GAP role of the device
+        :type role: BLE_GAP_ROLE
+        :return: result code
+        :rtype: BLE_ERROR
+        """
 
         error = self._ble_reset()
         if error == BLE_ERROR.BLE_STATUS_OK:
