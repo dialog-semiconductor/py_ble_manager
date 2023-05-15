62a1b79f58a0b4919c2d73bbe6a5efd8161ac080
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-05-15 18:29:30 -0400

    docstrings for BleApiBase

--- src/python_gtl_thread/ble_api/BleApiBase.py
+++ src/python_gtl_thread/ble_api/BleApiBase.py
@@ -3,6 +3,13 @@ from ..manager.BleManager import BleManager
 
 
 class BleApiBase():
+    """Base class for BLE API layer processing classes
+
+    :param ble_manager: BLE Manager for the framework
+    :type ble_manager: BleManager
+    :param ble_adapter: BLE adapter for the framework
+    :type ble_adapter: BleAdapter
+    """
     def __init__(self, ble_manager: BleManager, ble_adapter: BleAdapter):
         self._ble_manager = ble_manager
         self._ble_adapter = ble_adapter
