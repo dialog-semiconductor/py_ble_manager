c03bb37689b4b7c6758fbd0d1fe54dd750c5c96f
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-07-28 10:34:30 -0400

    Add connection timeout to example

--- examples/central/1_central_simple_connect/README.md
+++ examples/central/1_central_simple_connect/README.md
@@ -4,20 +4,26 @@ This example demonstrates creating/initializing a BleCentral object, connecting 
 
 You can run it with:
 
-`python central_simple_connect.py <com_port> <peripheral_addr>`
+`python central_simple_connect.py <com_port> <peripheral_addr> <timeout_s>`
 
-where `<com_port>` is the COM port associated with your development kit. Note, in the case of the Pro development kit there will be two COM ports associated with your development kit. You should use the lower of the two.
+`<com_port>` is the COM port associated with your development kit. Note, in the case of the Pro development kit there will be two COM ports associated with your development kit. You should use the lower of the two.
 
-And `<peripheral_addr>` is the address of the peripheral you want to connect to. \
+`<peripheral_addr>` is the address of the peripheral you want to connect to. \
 The address should be of the form 48:23:35:00:1b:53,P: \
 &emsp; where 48:23:35:00:1b:53 is the BLE device address and last letter indicates the address type: \
 &emsp;&emsp; P indicates a public address \
 &emsp;&emsp; R indicates a random address
 
+`<timeout_s>` is the length of time (in seconds) to wait for a connection to be established before cancelling the connection.
+
 Once running, you should see messages indicating you have connected to your peripheral:
 
 ![connection](assets/connection.png)
 
-After about 3 seconds, you should see a message indicating you have disconnected from your peripheral and the application will exit:
+About 3 seconds after a successful connection, you should see a message indicating you have disconnected from your peripheral and the application will exit:
 
 ![disconnection](assets/disconnection.png)
+
+If a connection is not established to the peripheral before the timeout, the connection will be cancelled and the application will exit:
+
+![timeout](assets/timeout.png)
--- examples/central/1_central_simple_connect/central_simple_connect.py
+++ examples/central/1_central_simple_connect/central_simple_connect.py
@@ -1,10 +1,16 @@
 import argparse
 import sys
 import time
+import threading
 import py_ble_manager as ble
 
 
-def main(com_port, peripheral_addr):
+def connection_timeout_cb(central: ble.BleCentral, timeout_s: int):
+    print(f"Peripheral not found in {timeout_s} seconds. Cancelling connection.")
+    central.connect_cancel()
+
+
+def main(com_port, peripheral_addr_str: str, timeout_s: int):
     central = ble.BleCentral(com_port)
 
     # Initialize the Pytohn BLE Framework
@@ -14,11 +20,16 @@ def main(com_port, peripheral_addr):
     central.start()
 
     # Convert the address string to a BdAddress
-    peripheral_addr: ble.BdAddress = ble.BleUtils.str_to_bd_addr(peripheral_addr)
+    peripheral_addr: ble.BdAddress = ble.BleUtils.str_to_bd_addr(peripheral_addr_str)
     # Set the connection parameters
     connection_params = ble.GapConnParams(interval_min_ms=50, interval_max_ms=70, slave_latency=0, sup_timeout_ms=420)
     # Initiate a connection
+    print(f"Sending connection request to: {peripheral_addr_str}")
     central.connect(peripheral_addr, connection_params)
+
+    # Create a timer to cancel the connection if the peripheral is not found
+    connection_timer = threading.Timer(timeout_s, connection_timeout_cb, [central, timeout_s])
+    connection_timer.start()
 
     conn_idx = 0
     while True:
@@ -33,14 +44,24 @@ def main(com_port, peripheral_addr):
                     conn_idx = evt.conn_idx
                     print(f"Connected to: addr={ble.BleUtils.bd_addr_to_str(evt.peer_address)}, conn_idx={evt.conn_idx}")
 
-                # Connection has completed. Print out the status, wait 3 seconds, and initiate a disconnect
+                # Connection has completed.
                 case ble.BLE_EVT_GAP.BLE_EVT_GAP_CONNECTION_COMPLETED:
                     evt: ble.BleEventGapConnectionCompleted
+                    # Print out the status
                     print(f"Connection completed: status={evt.status.name}")
 
-                    # Sleep for 3 seconds before disconnecting
-                    time.sleep(3)
-                    central.disconnect(conn_idx=conn_idx)
+                    if evt.status == ble.BLE_ERROR.BLE_STATUS_OK:
+                        # Successful connection
+
+                        # Cancel the connection timeout timer
+                        connection_timer.cancel()
+
+                        # Sleep for 3 seconds before disconnecting
+                        time.sleep(3)
+                        central.disconnect(conn_idx=conn_idx)
+                    else:
+                        # The connection was cancelled, exit
+                        sys.exit()
 
                 # Disconnected from peripheral
                 case ble.BLE_EVT_GAP.BLE_EVT_GAP_DISCONNECTED:
@@ -55,11 +76,13 @@ if __name__ == "__main__":
 
     parser.add_argument("com_port", type=str, help='COM port for your development kit')
 
-    parser.add_argument("peripheral_addr", help='Addresss of peripheral, e.g. 48:23:35:00:1b:53,P')
+    parser.add_argument("peripheral_addr", type=str, help='Addresss of peripheral, e.g. 48:23:35:00:1b:53,P')
+
+    parser.add_argument("timeout_s", type=int, help='Time (in seconds) to wait for a connection to be established before cancelling the connection')
 
     args = parser.parse_args()
 
     try:
-        main(args.com_port, args.peripheral_addr)
+        main(args.com_port, args.peripheral_addr, args.timeout_s)
     except KeyboardInterrupt:
         print("Exiting")
