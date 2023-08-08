5add913b5427ee6c8acbb9440598fe912aee6ef4
Patrick Murphy <patrick.murphy.jy@renesas.com>
2023-08-08 08:53:32 -0400

    remove unused import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-from importlib.metadata import version as get_version
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
