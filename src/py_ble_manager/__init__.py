d696d633dc0a2c796ae9c644c09ffabe2f8a94d6
Patrick Murphy <patrick.murphy.jy@renesas.com>
2025-02-26 18:37:49 -0500

    try/except for import modules supported thru optional dependency

--- src/py_ble_manager/__init__.py
+++ src/py_ble_manager/__init__.py
@@ -33,8 +33,14 @@ from .ble_devices.BlePeripheral import BlePeripheral
 from .gtl_port.gap import GAP_KDIST
 from .services.BleService import BleServiceBase, AttributeHandle, GattServiceDef, CharacteristicDef, DescriptorDef, GattCharacteristicDef
 
-from .tools.GtlAioSniffer import GtlAioSniffer
-from .tools.GtlSniffer import GtlSniffer
+# Only supported thru optional dependency
+try:
+    from .tools.GtlAioSniffer import GtlAioSniffer
+    from .tools.GtlSniffer import GtlSniffer
+except ImportError:
+    pass
+
+
 
 
 
